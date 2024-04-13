7f5d5eb97e3c1dd04bac703d01c60db6ea091f6a
Alison Chuang <119325354+alison-chuang@users.noreply.github.com>
2024-04-13 22:13:42 +0800

    test ci

--- webapp/tests/test_calculator.py
+++ webapp/tests/test_calculator.py
@@ -4,7 +4,7 @@ from webapp.app import app
 
 @pytest.fixture
 def client():
-    """Create a test client for the app."""
+    """Create a test client for the app"""
     with app.test_client() as client:
         yield client
 
